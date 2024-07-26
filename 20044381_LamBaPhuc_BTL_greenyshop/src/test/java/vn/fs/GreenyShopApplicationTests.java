2a58917587d237f2819ebcf8c545e7969f6efbd8
LamBaPhuc <lambaphuc@gmail.com>
2024-07-26 17:04:49 +0700

    BaiTapLon

---
+++ 20044381_LamBaPhuc_BTL_greenyshop/src/test/java/vn/fs/GreenyShopApplicationTests.java
@@ -0,0 +1,12 @@
+package vn.fs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenyShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
